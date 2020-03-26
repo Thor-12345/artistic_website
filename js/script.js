497b0515e654694a806a78aee223221e1c78233c
Thor W <6273158@rts-365.dk>
2020-03-26 10:08:06 +0100

    Finished draft 1.0

--- js/script.js
+++ js/script.js
@@ -19,3 +19,48 @@ document.querySelector('main').addEventListener('click', function(){
 })
 
 
+
+
+// WORKS GALLERY
+
+let lightbox = document.querySelector('#lightbox-wrapper');
+let images = document.querySelectorAll('.lightboxImages');
+
+window.onload = function(){
+    for (let i = 0; i < images.length; i++) {
+        images[i].addEventListener('click', function(){
+            lightbox.classList.remove('hidden');
+            document.querySelector('#lightboxImg').src = images[i].src;
+            });
+
+            let index = 0
+            
+            document.querySelector('#prev').addEventListener('click', function(){
+                index--;
+                if (index < 0) {
+                    index = images.length - 1;
+                }
+                document.querySelector('#lightboxImg').src = images[index].src;
+            })
+
+            document.querySelector('#next').addEventListener('click', function(){
+                index++;
+                if (index > images.length - 1) {
+                    index = 0;
+                }
+                document.querySelector('#lightboxImg').src = images[index].src;
+            })
+
+            lightbox.addEventListener('click', function(){
+                lightbox.classList.add('hidden'); 
+            })
+    }
+}
+
+document.querySelector('#lightboxDiv').addEventListener('click', function(){
+    event.stopPropagation()
+})
+
+
+
+
